00ebcdc5bf244cca4a5c9215d8b932a423a5e26a
Akshay Gupta <akshgupta547@gmail.com>
2017-05-11 08:40:07 +0530

    Http, Curd and Search features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module'
+
+//in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent }  from './heroes.component';
 import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from "./hero-detail.component";
 import { HeroService } from "./hero.service";
+import { HeroSearchComponent } from './hero-search.component'
 
 
 @NgModule({
   imports:      [
 		BrowserModule,
 		FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
 		AppRoutingModule
 	],
 
@@ -22,7 +30,8 @@ import { HeroService } from "./hero.service";
 		AppComponent,
 		DashboardComponent,
 		HeroesComponent,
-		HeroDetailComponent
+		HeroDetailComponent,
+    HeroSearchComponent
 	],
   providers: [
 		HeroService
--- src/app/hero-detail.component.js
+++ src/app/hero-detail.component.js
@@ -30,6 +30,11 @@ var HeroDetailComponent = (function () {
         })
             .subscribe(function (hero) { return _this.hero = hero; });
     };
+    HeroDetailComponent.prototype.save = function () {
+        var _this = this;
+        this.heroservice.update(this.hero)
+            .then(function () { return _this.goBack(); });
+    };
     HeroDetailComponent.prototype.goBack = function () {
         this.location.back();
     };
